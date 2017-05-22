15157d99bb4b6fffb5c137e8a2a6623130d57451
omaotzu <omarhphillips@gmail.com>
2017-05-22 13:15:27 +0100

    clean and css gulp tasks added

---
+++ tasks/css.js
@@ -0,0 +1,46 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const autoprefixer = require('gulp-autoprefixer');
+const stripCssComments = require('gulp-strip-css-comments');
+const minifyCss = require('gulp-minify-css');
+const bowerFiles = require('main-bower-files');
+const concat = require('gulp-concat');
+const eventStream = require('event-stream');
+const order = require('gulp-order');
+const sourcemaps = require('gulp-sourcemaps');
+const rename = require('gulp-rename');
+const gulpIf = require('gulp-if');
+const browserSync = require('browser-sync');
+const config = require('../package').gulp;
+
+const fetchVendorCss = () => {
+  return gulp.src(bowerFiles(config.selectors.css))
+    .pipe(stripCssComments())
+    .pipe(concat(config.vendors.css));
+};
+
+const fetchLocalCss = () => {
+  return gulp.src(`${config.src.csss}${config.main.css}`)
+    .pipe(sass({style: 'expanded'}))
+    .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
+    .pipe(concat(config.output.css));
+};
+
+const buildCss = () => {
+  const vendorCss = fetchVendorCss();
+  const localCss = fetchLocalCss();
+
+  return eventStream.merge(vendorCss, localCss)
+    .pipe(order[config.vendor.css, config.output.css])
+    .pipe(concat(config.output.css))
+    .pipe(sourcemaps.init())
+    .pipe(gulpIf(global.production, minifyCss()))
+    .pipe(gulpIf(global.production, rename({ suffix: '.min'})))
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest(config.dest.css))
+    .pipe(gulpIf(!global.production, browserSync.stream()));
+};
+
+
+gulp.task('build-css', buildCss);
+module.exports = buildCss;
